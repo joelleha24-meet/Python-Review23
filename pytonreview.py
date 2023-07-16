816bdd059d1586add327dca2bb5cc457a16c1d0f
joelleha24-meet <139680897+joelleha24-meet@users.noreply.github.com>
2023-07-16 18:58:54 +0300

    Update pytonreview.py
    im working on it

---
+++ pytonreview.py
@@ -0,0 +1,13 @@
+def create_youtube_video(title,description):
+	dic = {"title": title,"description": description,"likes":0,"dislikes":0,c"omments":{}}
+	return dic
+
+def like(video):
+	if["likes"]
+
+video = create_youtube_video({"hi","hey"})
+like(video)
+
+
+
+
